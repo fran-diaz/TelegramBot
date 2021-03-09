0cc6c07577747c2b237f688e88693ecdfa6cede7
Fran Díaz <fran.diaz.gonzalez@gmail.com>
2021-03-09 13:33:17 +0100

    Update webhook-dev.php

--- webhook-dev.php
+++ webhook-dev.php
@@ -1,6 +1,6 @@
 <?php
 
-use TelegramBot\TelegramBot;
+
 
 require('config/development.php');
 require('TelegramBotAPI/TelegramBot.php');
@@ -8,6 +8,7 @@ require('TelegramBotAPI/TelegramBot.php');
 //require('ITE/iteLite.php');
 //require('ITE/dbInterface.php');
 //require('ITE/mysql.php');
+use TelegramBotAPI\TelegramBot;
 
 //$_ITE = new ITE\ite();
 //$_ITE->bdd = new ITE\db\mysql($_ITE);
