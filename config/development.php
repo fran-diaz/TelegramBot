5cf3bb51274623aaca2d5b86a8a4df748d72d876
Fran Díaz <fran.diaz.gonzalez@gmail.com>
2021-03-10 17:31:27 +0100

    Update development.php

--- config/development.php
+++ config/development.php
@@ -1,12 +1,8 @@
 <?php
 
 /**
- * WEBHOOK URI: https://api.telegram.org/bot1689780893:AAHG36iUBXO5EJlhD4PMwzwtgR1Zc98wHE0/setWebhook?url=https://app1.brainhardware.es/resources/TelegramBot/webhook-dev.php
+ * WEBHOOK URI: https://api.telegram.org/bot1689780893:AAHG36iUBXO5EJlhD4PMwzwtgR1Zc98wHE0/setWebhook?url=[DOMAIN]/resources/TelegramBot/webhook-dev.php
  */
-
-function decode($data) {
-    return json_decode(gzinflate(base64_decode(strtr($data, '-_,', '+/='))),true);
-}
 
 define("BOT_NAME", decode('U3JwDAiIdypKzMyLT8ovUQIA'));
 define('BOT_TOKEN', decode('UzI0s7A0tzCwsDS2cnT0cDc2ywx1ivA3dfXKyXAxCfAtryovSQ8yjEq2tCj3cDVQAgA,'));
