690c63d655dcd381f65e4f1ebc08dbf90901d02d
Fran Díaz <fran.diaz.gonzalez@gmail.com>
2021-03-10 10:26:45 +0100

    Update webhook-dev.php

--- webhook-dev.php
+++ webhook-dev.php
@@ -6,3 +6,10 @@ use TelegramBotAPI\TelegramBot;
 
 $bot = new TelegramBot();
 $bot -> init( 'webhook' );
+
+ob_start();
+var_dump($_SESSION,$_ITE);
+$buffer = ob_get_contents();
+ob_end_clean();
+
+file_put_contents( 'prueba-log.txt',  $buffer ."\n", FILE_APPEND );
