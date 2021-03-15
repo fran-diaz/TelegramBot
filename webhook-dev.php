33b3185de9f3f7abd768e4a3aa9ab31a15be4804
Fran Díaz <fran.diaz.gonzalez@gmail.com>
2021-03-15 09:49:54 +0100

    Update webhook-dev.php

--- webhook-dev.php
+++ webhook-dev.php
@@ -9,17 +9,22 @@ foreach ( rglob( "TelegramBotAPI/*.php" ) as $filename ) {
 	require( $filename );
 }
 
+
+
+ob_start();
+
+// Required files
+foreach ( rglob( "TelegramBotAPI/*.php" ) as $filename ) {
+	echo( $filename );
+}
+
+$buffer = ob_get_contents();
+ob_end_clean();
+
+file_put_contents( 'prueba-log.txt',  $buffer ."\n", FILE_APPEND );
+
 // Bot initializing
 use TelegramBotAPI\TelegramBot;
 
 $bot = new TelegramBot();
 $bot -> init( 'webhook' );
-
-/*ob_start();
-
-var_dump($db -> info());
-
-$buffer = ob_get_contents();
-ob_end_clean();
-
-file_put_contents( 'prueba-log.txt',  $buffer ."\n", FILE_APPEND );*/
