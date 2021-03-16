234ab81a00027d7f35a2435c756a0acc9f19368b
Fran Díaz <fran.diaz.gonzalez@gmail.com>
2021-03-16 09:54:10 +0100

    Update sendMessage.php

--- TelegramBotAPI/methods/sendMessage.php
+++ TelegramBotAPI/methods/sendMessage.php
@@ -4,7 +4,7 @@ namespace TelegramBotAPI\methods;
 trait sendMessage {
 	public function sendMessage( $text, $chat_id = null, $msg_id = null ){
 		$chat_id = ( ! empty( $this -> chat_id ) ) ? $this -> chat_id : null;
-		$chat_id = ( ! empty( $this -> msg_id ) ) ? $this -> msg_id : null;
+		$msg_id = ( ! empty( $this -> msg_id ) ) ? $this -> msg_id : null;
 
         $data = [
             'chat_id' => $chat_id,
