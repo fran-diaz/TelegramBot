37a63298948e3c3a6b5b8cbccd186835d908f221
Fran Díaz <fran.diaz.gonzalez@gmail.com>
2021-03-16 12:16:49 +0100

    Update sendDocument.php

--- TelegramBotAPI/methods/sendDocument.php
+++ TelegramBotAPI/methods/sendDocument.php
@@ -3,9 +3,12 @@ namespace TelegramBotAPI\methods;
 
 trait sendDocument {
 	public function sendDocument( $document_url, $chat_id = null, $msg_id = null ){
-        var_dump($chat_id);
-		$chat_id = ( is_null($chat_id) && ! empty( $this -> chat_id ) ) ? $this -> chat_id : null;
-		$msg_id = ( is_null($msg_id) && ! empty( $this -> msg_id ) ) ? $this -> msg_id : null;
+		if ( is_null($chat_id) && ! empty( $this -> chat_id ) ){
+            $chat_id = $this -> chat_id;
+        }
+		if ( is_null($msg_id) && ! empty( $this -> msg_id ) ) {
+            $msg_id = $this -> msg_id;
+        }
 
         $data = [
             'chat_id' => $chat_id,
@@ -17,7 +20,7 @@ trait sendDocument {
             $data['reply_to_message_id'] = $msg_id;
             $data['allow_sending_without_reply'] = true;
         }
-        var_dump($data);
+        
         
         $result = $this -> rest( 'sendDocument', $data );
         $this -> log( 'sent-file', json_encode( $data ));
