b81743e0f6544492fb9a96eb77d1ea2b6452bcc7
Fran Díaz <fran.diaz.gonzalez@gmail.com>
2021-03-16 11:56:52 +0100

    Create sendDocument.php

---
+++ TelegramBotAPI/methods/sendDocument.php
@@ -0,0 +1,26 @@
+<?php
+namespace TelegramBotAPI\methods;
+
+trait sendDocument {
+	public function sendDocument( $document_url, $chat_id = null, $msg_id = null ){
+		$chat_id = ( ! empty( $this -> chat_id ) ) ? $this -> chat_id : null;
+		$msg_id = ( ! empty( $this -> msg_id ) ) ? $this -> msg_id : null;
+
+        $data = [
+            'chat_id' => $chat_id,
+            'parse_mode' => 'HTML',
+            'document' => $document_url,
+        ];
+
+        if( ! is_null( $msg_id ) ){
+            $data['reply_to_message_id'] = $msg_id;
+            $data['allow_sending_without_reply'] = true;
+        }
+        
+        $result = $this -> rest( 'sendDocument', $data );
+        $this -> log( 'sent', json_encode( $data ));
+        $this -> log( 'sent', 'message_id: '. $msg_id );
+        $this -> log( 'sent', 'Result: '. json_encode($result) );
+        return $result;
+    }
+}
