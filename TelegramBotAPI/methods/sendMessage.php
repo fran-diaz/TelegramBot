5581ddf28c8615c6d2162d3df5525d1ae1fcc58c
Fran Díaz <fran.diaz.gonzalez@gmail.com>
2021-03-16 09:52:49 +0100

    Update sendMessage.php

--- TelegramBotAPI/methods/sendMessage.php
+++ TelegramBotAPI/methods/sendMessage.php
@@ -20,7 +20,7 @@ trait sendMessage {
         $result = $this -> rest( 'sendMessage', $data );
         $this -> log( 'sent', json_encode( $data ));
         $this -> log( 'sent', 'message_id: '. $msg_id );
-        $this -> log( 'sent', 'Result: '. $result );
+        $this -> log( 'sent', 'Result: '. json_encode($result) );
         return $result;
     }
 }
