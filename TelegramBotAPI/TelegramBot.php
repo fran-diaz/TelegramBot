e2467dbcc55567b36752b35d7dd55d66f636cabc
Fran Díaz <fran.diaz.gonzalez@gmail.com>
2021-03-17 12:50:52 +0100

    Update TelegramBot.php

--- TelegramBotAPI/TelegramBot.php
+++ TelegramBotAPI/TelegramBot.php
@@ -90,7 +90,7 @@ class TelegramBot {
     private function rest( string $method, array $json ) {
         $ch = curl_init();
         $result = null;
-        
+        $this -> log( '/home/app1/public_html/resources/rest-request', API_URL . $method . '?'.http_build_query($json)  );
 
         $postdata = http_build_query( $json );
 
@@ -104,9 +104,8 @@ class TelegramBot {
 
         $context  = stream_context_create($opts);
 
-        $result = file_get_contents( API_URL . $method, false, $context );
-        $this -> log( '/home/app1/public_html/resources/rest-result', $result  );
-        return $result;
+
+        return file_get_contents( API_URL . $method, false, $context );
 
         curl_setopt( $ch, CURLOPT_URL, API_URL . $method . '?chat_id='.$json['chat_id'] ); 
         try {
