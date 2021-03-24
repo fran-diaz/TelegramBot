6b2346f7a227304170677e15d119b4333f1d2137
Fran Díaz <fran.diaz.gonzalez@gmail.com>
2021-03-24 12:38:31 +0100

    Update TelegramBot.php

--- TelegramBotAPI/TelegramBot.php
+++ TelegramBotAPI/TelegramBot.php
@@ -101,8 +101,7 @@ class TelegramBot {
             curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
             
             curl_setopt( $ch, CURLOPT_HTTPHEADER, [
-                'Content-Type: application/json',
-                'Content-Length: ' . strlen( $data_string ) 
+                'Content-Type: application/json', 
             ]);
 
             $result = json_decode( curl_exec( $ch ), true );
