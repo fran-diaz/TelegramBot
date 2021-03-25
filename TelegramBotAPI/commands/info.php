6f58d65bd6de339c095dd86a5eddeaee24fcf5c5
Fran Díaz <fran.diaz.gonzalez@gmail.com>
2021-03-25 10:54:20 +0100

    Update info.php

--- TelegramBotAPI/commands/info.php
+++ TelegramBotAPI/commands/info.php
@@ -2,19 +2,19 @@
 namespace TelegramBotAPI\commands;
 
 trait info {
-    public function info( $args = null ){
-        if( is_null( $args ) ){
+    public function info( $id = null ){
+        if( is_null( $id ) ){
             $this -> sendMessage( 'Tienes que ser más específico.' );
             return false;
         }
 
-        $user_info = $this -> db -> select('TelegramBot__users','*',['user' => $this -> input["message"]["from"]["first_name"]]);
+        $user_info = $this -> db -> get('TelegramBot__users','*',['user' => $this -> input["message"]["from"]["first_name"]]);
         if( ! $user_info ){
             $this -> sendMessage( 'No nos consta que estés registrado.' );
             return false;
         }
 
-        $info = $this -> db -> select('_intervenciones','*',['id_intervencion' => $args, 'telegram_usr_id' => $user_info['TelegramBot__users_id']]);
+        $info = $this -> db -> get('_intervenciones','*',['id_intervencion' => $id, 'telegram_usr_id' => $user_info['TelegramBot__users_id']]);
         if( $interv_info ){
             $reply = "";
             $reply .= "Intervención número <strong>".$info['id_intervencion']."</strong>, ".$info['estado'].".\n";
