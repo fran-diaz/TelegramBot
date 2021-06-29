1b42ea6d2808ca3a880c054d0e03b90d830cec2e
Fran Díaz <fran.diaz.gonzalez@gmail.com>
2021-06-29 11:40:58 +0200

    Update new_user.php
    Groups messages supported and groups table creating

--- TelegramBotAPI/methods/new_user.php
+++ TelegramBotAPI/methods/new_user.php
@@ -7,11 +7,26 @@ trait new_user {
         
         $values = [];
 
-        if( $this -> check_users_table() ) {
-            if( empty( $this -> input ) ) {
+        if( empty( $this -> input ) ) {
+            return false;
+        }
+
+        if( $this -> input["message"]["chat"]["type"] === 'group' ){
+            if( $this -> check_groups_table() ) {
+                if( $this -> db -> has( 'TelegramBot__groups', ['chat_id' => $this -> input["message"]["chat"]["id"]] ) ){
+                    return true;
+                } else {
+                    $values['chat_id'] = $this -> input["message"]["chat"]["id"];
+                    $values['title'] = $this -> input["message"]["chat"]["title"];
+                
+                    return $this -> db -> insert( 'TelegramBot__groups', $values );
+                }
+            } else {
                 return false;
             }
+        }
 
+        if( $this -> check_users_table() ) {
             if( $this -> db -> has( 'TelegramBot__users', ['user' => $this -> input["message"]["from"]["first_name"]] ) ){
                 return true;
             } else {
@@ -42,6 +57,27 @@ trait new_user {
                   `chat_id` int(11) UNSIGNED NOT NULL,
                   `active` bit(1) NULL DEFAULT b'0',
                   PRIMARY KEY (`TelegramBot__users_id`) USING BTREE
+                ".' ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;SET FOREIGN_KEY_CHECKS=1;') -> fetchAll();
+        }
+
+        return true;
+    }
+
+    private function check_groups_table(){
+        if( empty( $this -> db ) ){
+            return false;
+        }   
+
+        // Registro los nuevos grupos
+        $aux = $this -> db -> query("SHOW TABLES LIKE 'TelegramBot__groups'") -> fetchAll();
+
+        if( empty( $aux ) ) {
+            $this -> db -> query('SET FOREIGN_KEY_CHECKS=0; CREATE TABLE `TelegramBot__groups` ( '."
+                `TelegramBot__groups_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'column:no,form:no',
+                  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NULL DEFAULT NULL,
+                  `chat_id` int(11) NOT NULL,
+                  `active` bit(1) NULL DEFAULT b'0',
+                  PRIMARY KEY (`TelegramBot__groups_id`) USING BTREE
                 ".' ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;SET FOREIGN_KEY_CHECKS=1;') -> fetchAll();
         }
 
